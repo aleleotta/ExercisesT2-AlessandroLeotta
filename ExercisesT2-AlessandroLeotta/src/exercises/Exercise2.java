57bc1278fbbfb3b8703cbddb75821bd45eb9caac
aleleotta <aleleotta@106-02.info2>
2022-10-18 12:44:52 +0200

    Exercise 2 in progress!

---
+++ ExercisesT2-AlessandroLeotta/src/exercises/Exercise2.java
@@ -0,0 +1,10 @@
+package exercises;
+import java.util.*;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		int dni;
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+	}
+}
