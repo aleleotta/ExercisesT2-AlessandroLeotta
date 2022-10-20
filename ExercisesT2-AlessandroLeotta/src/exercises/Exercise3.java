5e1786cae8cc6a0f743880b9f33dbcf09fba2818
aleleotta <aleleotta@106-02.info2>
2022-10-20 14:10:08 +0200

    Exercise 3 in progress!

--- ExercisesT2-AlessandroLeotta/src/exercises/Exercise3.java
+++ ExercisesT2-AlessandroLeotta/src/exercises/Exercise3.java
@@ -4,12 +4,20 @@ import java.util.*;
 public class Exercise3 {
 	public static void main(String[] args) {
 		int animals;
-		double foodKg;
+		double foodUnits;
+		double foodKg = 1.0;
+		double result;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce the amount of animals that are being fed in the barn: ");
 		animals = sc.nextInt();
 		System.out.println("Introduce the food kilograms that are consumed by the animals: ");
 		foodKg = sc.nextDouble();
+		result = foodKg / animals;
+		if (foodKg >= animals) {
+			System.out.println("There is enough food for all animals. Amount of food per animal: " + result + "kg");
+		} else if (foodKg < animals) {
+			System.out.println("The food units are gonna have to be split in rations to distribute it equally per animal. Amount of food per animal: " + result + "kg");
+		}
 		sc.close();
 	}
 }
