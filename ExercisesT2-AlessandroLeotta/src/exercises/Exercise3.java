3db242dab73241c891d297135bede0cc4f6c77d2
aleleotta <aleleotta@106-02.info2>
2022-10-20 13:34:53 +0200

    Exercise 3 initiated!

--- ExercisesT2-AlessandroLeotta/src/exercises/Exercise3.java
+++ ExercisesT2-AlessandroLeotta/src/exercises/Exercise3.java
@@ -3,14 +3,12 @@ import java.util.*;
 
 public class Exercise3 {
 	public static void main(String[] args) {
-		int foodAmount, animals;
+		int animals;
 		double foodKg;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce the amount of animals that are being fed in the barn: ");
 		animals = sc.nextInt();
-		System.out.println("Introduce the amount of food in units that are being consumed by the animals: ");
-		foodAmount = sc.nextInt();
-		System.out.println("Introduce the food kilograms that are bought: ");
+		System.out.println("Introduce the food kilograms that are consumed by the animals: ");
 		foodKg = sc.nextDouble();
 		sc.close();
 	}
